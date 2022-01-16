9a3548480366162c8ec94539e51567e0c442cf89
nbn <nbn1209@126.com>
2022-01-16 23:50:37 +0800

    feat: [中位数]

--- Array/findMedianSortedArrays.ts
+++ Array/findMedianSortedArrays.ts
@@ -21,3 +21,60 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
   return result
 
 };
+
+
+/**
+ * 双指针求解
+ * @param nums1 
+ * @param nums2 
+ * @returns 
+ */
+function findMedianSortedArrays2(nums1: number[], nums2: number[]): number {
+
+  /**
+   * 思路：
+   * 
+   * 两个指针分别指向两个数组，
+   * 将两个数组合并成一个大的有序数组
+   * 根据奇偶性得到中位数
+   * 
+   */
+  const result: number[] = []
+
+
+  const mid = Math.floor((nums1.length + nums2.length) / 2)
+  let i = 0; let j = 0
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] <= nums2[j]) {
+      result.push(nums1[i])
+      i++
+    } else {
+      result.push(nums2[j])
+      j++
+
+    }
+
+  }
+
+  if (i < nums1.length) {
+    while (i < nums1.length) {
+      result.push(nums1[i])
+      i++
+    }
+  } else if (j < nums2.length) {
+    while (j < nums2.length) {
+      result.push(nums2[j])
+      j++
+    }
+  }
+
+
+  // console.log(result,mid)
+  if (result.length % 2 == 0) {
+    return (result[mid] + result[mid - 1]) / 2
+  } else {
+    return result[mid]
+  }
+
+};
